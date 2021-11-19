f0348658dd6c250db34401af6847a6fedfd4a578
Dinu-Senal <60167570+Dinu-Senal@users.noreply.github.com>
2021-11-19 09:22:46 +0530

    todo detail part

--- Shared/Todo.cs
+++ Shared/Todo.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BlazorCRUDWeb.Shared
 {
-    class Todo
+    public class Todo
     {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        public String Name { get; set; }
+
+        public String Description { get; set; }
     }
 }
